446862aa0615c1453ac0b9915986565d10520c31
Miszo Radomski <miszo.radomski@gmail.com>
2019-04-07 12:39:18 +0200

    Feat: Add calendar validator

--- test/calendar.router.test.js
+++ test/calendar.router.test.js
@@ -1,13 +1,17 @@
-const express = require('express')
+const express = require('express');
 const supertest = require('supertest');
+const Ajv = require('ajv');
 
 const router = require('../web/routing/calendar.router');
 
 let app = null;
+let ajv = null;
 
 beforeEach(() => {
   app = express();
   router(app);
+
+  ajv = new Ajv();
 });
 
 it('api/calendar exists', async () => {
@@ -18,6 +22,18 @@ it('api/calendar exists', async () => {
     .get('/api/calendar')
     .expect(200);
 
-  expect(res.body.status).toBe('OK');
 });
 
+it('/api/calendar response is valid', async () => {
+  const schema = require('../docs/schemas/calendar.scheme');
+  const validate = ajv.compile(schema);
+
+  const res = await supertest(app)
+    .get('/api/calendar?month=???')
+    .expect(200);
+
+  const valid = validate(res.body);
+  console.log(validate.errors);
+  expect(valid).toBeTruthy();
+  expect(validate.errors).toBeNull();
+});
--- web/routing/calendar.router.js
+++ web/routing/calendar.router.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/api/calendar', (req, res) => res.status(200).json({ status: 'OK' }));
+router.get('/api/calendar', (req, res) =>
+  res
+    .status(200)
+    .json({
+      data: [{
+        date: '2019-01-01',
+        events: [{
+          id: 'asdgdfsgdfs',
+          title: 'Potato'
+        }]
+      }]
+    }));
 
 module.exports = (app) => {
   app.use(router);
